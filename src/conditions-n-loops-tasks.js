170df8bda46944ac51c701317686e61992c950de
Artur Allamyradov <allamuradov@internetlab.ru>
2024-01-12 23:17:18 +0300

    feat: implement part of task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -285,8 +285,13 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -304,8 +309,16 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let numCopy = num;
+  while (numCopy > 0) {
+    if (numCopy % 10 === digit) {
+      return true;
+    }
+    numCopy = Math.floor(numCopy / 10);
+  }
+
+  return false;
 }
 
 /**
